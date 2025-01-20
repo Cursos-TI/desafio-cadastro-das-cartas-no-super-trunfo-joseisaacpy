75e23f1ad00d456dd84ff332a99926f7e563cd16
José Isaac <joseisaacnascimento@gmail.com>
2025-01-20 15:34:22 -0300

    Declara todas as variáveis.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,19 +4,23 @@
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
 
-int main() {
+int main()
+{
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
-    return 0;
+    // código da cidade
+    int cod_cidade;
+    // nome
+    char nome_cidade;
+    // população
+    float populacao_cidade;
+    // área
+    double area_cidade;
+    // PIB
+    double pib_cidade;
+    // número de pontos turísticos
+    int num_pontos_tur_cidade;
+
 }
